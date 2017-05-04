aaa1c5cefb1386514540d0ddc969cc2e98d176cc
spikespaz <spikespaz@outlook.com>
2017-05-03 17:33:01 -0700

    Intro toggleable

--- application.js
+++ application.js
@@ -1,5 +1,15 @@
 hide_intro = document.getElementById("hide-intro");
 
+var intro_hidden = false;
+
 hide_intro.onclick = function() {
-    document.getElementById("intro").style.display = "none";
+    if (intro_hidden) {
+        document.getElementById("intro").style.display = "block";
+        hide_intro.textContent = "Ok, hide this please!";
+        intro_hidden = false;
+    } else {
+        document.getElementById("intro").style.display = "none";
+        hide_intro.textContent = "Show me the intro!";
+        intro_hidden = true;
+    }
 };
