117951d6b17c2598bf7db56a878aa3ae5def8cc2
spikespaz <spikespaz@outlook.com>
2017-05-04 10:45:31 -0700

    Live format text

--- application.js
+++ application.js
@@ -1,5 +1,6 @@
-HIDE_INTRO = document.getElementById("hide-INTRO");
-INTRO = document.getElementById("INTRO");
+HIDE_INTRO = document.getElementById("hide-intro");
+INTRO = document.getElementById("intro");
+STORY = document.getElementById("story-edit");
 
 var intro_hidden = false;
 
@@ -12,5 +13,11 @@ HIDE_INTRO.onclick = function() {
         INTRO.style.display = "none";
         HIDE_INTRO.textContent = "Show me the intro!";
         intro_hidden = true;
+    }
+};
+
+STORY.onkeyup = function(event) {
+    if (event.which === 221) {
+        STORY.innerHTML = STORY.innerText.replace(/\[([\w\s]+)]/g, "<code>[$1]</code> ");
     }
 };
