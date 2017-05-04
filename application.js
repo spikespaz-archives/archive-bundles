bbe002b2f75d9c24a45a187db8f7a5de268ba14d
spikespaz <spikespaz@outlook.com>
2017-05-04 10:46:10 -0700

    Use "this"

--- application.js
+++ application.js
@@ -7,17 +7,17 @@ var intro_hidden = false;
 HIDE_INTRO.onclick = function() {
     if (intro_hidden) {
         INTRO.style.display = "block";
-        HIDE_INTRO.textContent = "Ok, hide this please!";
+        this.textContent = "Ok, hide this please!";
         intro_hidden = false;
     } else {
         INTRO.style.display = "none";
-        HIDE_INTRO.textContent = "Show me the intro!";
+        this.textContent = "Show me the intro!";
         intro_hidden = true;
     }
 };
 
 STORY.onkeyup = function(event) {
     if (event.which === 221) {
-        STORY.innerHTML = STORY.innerText.replace(/\[([\w\s]+)]/g, "<code>[$1]</code> ");
+        this.innerHTML = STORY.innerText.replace(/\[([\w\s]+)]/g, "<code>[$1]</code> ");
     }
 };
