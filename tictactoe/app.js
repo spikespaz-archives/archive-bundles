0b3cf5ea97516d9bebd3cf02b123a9b5ad8d3979
Jacob <spikespaz@outlook.com>
2017-05-18 22:55:29 +0000

    Proper seconds

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -2,12 +2,7 @@ const table = document.getElementById("grid");
 const turn = document.getElementById("turn").lastChild;
 const time = document.getElementById("time").lastChild;
 
-const counter = setInterval(() => {
-    centiseconds++;
-    time.innerText = getSeconds() + "s";
-}, 100);
-
-let next = "x", grid = [], centiseconds = 0, row, cell, index;
+let next = "x", grid = [], seconds = 0, row, cell, index;
 
 function randomInt(min, max) {
     min = Math.ceil(min);
@@ -19,10 +14,6 @@ for (row = 0; row < table.rows.length; row++) {
     for (cell = 0; cell < table.rows[row].cells.length; cell++) {
         grid.push(table.rows[row].cells[cell])
     }
-}
-
-function getSeconds() {
-    return Math.floor(centiseconds / 10)
 }
 
 function updateCell(cell) {
@@ -38,7 +29,7 @@ function updateCell(cell) {
     }
 }
 
-setTimeout(() => {
-    clearTimeout(counter);
-    location.reload()
-}, 80000);
+setInterval(() => {
+    seconds++;
+    time.innerText = seconds + "s";
+}, 1000);
