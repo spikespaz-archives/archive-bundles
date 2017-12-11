e1c4315e7181639deb74e8d1e51f0b2c33c0bfcb
spikespaz <spikespaz@outlook.com>
2017-12-10 22:32:17 -0700

    Added optional top_pad argument to figlet print and fixed file logger

--- lib_common.py
+++ lib_common.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-from time import time
+from time import strftime
 from pyfiglet import figlet_format
 from os import system, name as os_name, popen
 
@@ -10,14 +10,14 @@ def clear():
 
 
 # Function to pretty print to the perfect center of a console window
-def fullscreen_print(text, font="dotmatrix"):
+def fullscreen_print(text, font="dotmatrix", top_pad=0):
     size = popen("stty size", "r").read().split()  # Make a tuple for the size of the console
     size = int(size[1]), int(size[0])  # Switch them, make it W, H with ints
 
     fig = figlet_format(str(text), font=font, justify="center", width=size[0])  # Get the fig formatted string
 
     fig_height = len(fig.split("\n"))  # Get the line length of the figlet formatted string
-    top_pad = round((size[1] - fig_height) / 2) + 3  # Calculate top padding (newlines) to put before the fig
+    top_pad += round((size[1] - fig_height) / 2)  # Calculate top padding (newlines) to put before the fig
 
     clear()  # Clear the screen
     print("\n" * top_pad, fig)  # Print the figlet and padding
@@ -26,13 +26,13 @@ def fullscreen_print(text, font="dotmatrix"):
 # Write a line to a log file with args
 def log_line(log_file, text, level=None, indent=0):
     line_text = (" " * indent +
-                 time().strftime("[%m-%d-%y | %I:%M:%S %p]") +
-                 (" [" + level.upper() + "]") if level else "" +
-                 ": " + text)
+                 strftime("[%m-%d-%y | %I:%M:%S %p]") +
+                 ((" [" + level.upper() + "]") if level else "") +
+                 ": " + text + "\n")
 
     if log_file.mode in ("a", "a+"):
         log_file.write(line_text)
-    elif log_file.mode in ("w", "w+"):
-        log_file.write(log_file.read() + "\n" + line_text)
+    elif log_file.mode == "w+":
+        log_file.write(log_file.read() + line_text)
     else:
         raise FileNotFoundError("File must be write-able.")
