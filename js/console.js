36d06ce0ee4e43dccb65405d241684ac99b856b7
spikespaz <spikespaz@outlook.com>
2018-01-06 03:28:24 -0700

    Restructure JavaScript

--- js/console.js
+++ js/console.js
@@ -1,30 +1,79 @@
-function nextInputLine(buffer) {
-    var buffer_line = document.createElement("li");
-    buffer_line.className = "console-buffer-line";
-
-    var input_line = document.createElement("p");
-    input_line.className = "console-input-line";
-    input_line.innerHTML = "<span class='cursor blink'></span>"
-
-    buffer_line.appendChild(input_line);
-    buffer.appendChild(buffer_line);
-
-    return input_line
+function stringSplice(original, position, remove, replacement) {
+    return original.slice(0, position) + replacement + original.slice(position + Math.abs(remove))
 }
 
 
 function registerConsole(console_element) {
-    var input_box = document.createElement("input");
-    input_box.setAttribute("class", "console-input-box");
-    input_box.setAttribute("type", "text");
-    console_element.appendChild(input_box);
+    var console_buffer = console_element.getElementsByClassName("console-buffer")[0];
+    // var console_lines = console_buffer.getElementsByClassName("console-line");
+    var console_input = document.createElement("input");
+    var console_display = nextDisplayLine(console_buffer);
 
-    console_element.onclick = function() {input_box.focus()};
-    console_element.onblur = function() {};
+    console_input.setAttribute("class", "console-input");
+    console_input.setAttribute("type", "text");
 
-    var buffer = console_element.getElementsByClassName("console-buffer-list")[0];
+    console_element.appendChild(console_input);
 
-    var current_line = nextInputLine(buffer);
+    function setDisplayLine(value) {
+        console_display.innerHTML = value;
+    }
+
+    function getSelectionRange() {
+        return [console_input.selectionStart, console_input.selectionEnd];
+    }
+
+    function getCursorElement() {
+        return console_display.getElementsByClassName("console-cursor")[0];
+    }
+
+    function getInputNoWrap() {
+        return console_input.value.replace(/ /g, "\xa0");
+    }
+
+    function getRenderedSelection() {
+        var selection_range = getSelectionRange();
+        var input_value = getInputNoWrap()
+
+        if (selection_range[0] == selection_range[1])
+            return stringSplice(input_value, selection_range[0], 0, "<span class='console-cursor'></span>");
+        else
+            return stringSplice(input_value, selection_range[0], selection_range[1] - selection_range[0],
+                                "<mark>" + input_value.slice(selection_range[0], selection_range[1]) + "</mark>")
+    }
+
+    function nextDisplayLine() {
+        var console_line = document.createElement("li");
+        console_line.classList.add("console-line");
+
+        var console_display = document.createElement("p");
+        console_display.classList.add("console-display");
+
+        console_line.appendChild(console_display);
+        console_buffer.appendChild(console_line);
+
+        return console_display
+    }
+
+    console_element.onclick = function() {
+        setDisplayLine(getRenderedSelection());
+        console_input.focus();
+    };
+
+    console_input.onblur = function() {
+        setDisplayLine(getInputNoWrap());
+    };
+
+    console_element.onkeyup = function() {
+        setDisplayLine(getRenderedSelection());
+    };
+
+    console_element.onkeydown = function() {
+        setDisplayLine(getRenderedSelection());
+    };
+
+    console_element.onkeypress = function() {
+        setDisplayLine(getRenderedSelection());
+    };
 }
 
 
