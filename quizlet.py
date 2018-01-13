f7d8bf108d6b4b6105f1cf13b6a810a53121b7c8
spikespaz <spikespaz@outlook.com>
2018-01-13 12:05:01 -0700

    Move QStatistics to user class

--- quizlet.py
+++ quizlet.py
@@ -127,23 +127,22 @@ class QSearch:
 
 
 class QUser:
+    class QStatistics:
+        def __init__(self):
+            self.study_session_count = 0
+            self.total_answer_count = 0
+            self.public_sets_created = 0
+            self.public_terms_entered = 0
+            self.total_sets_created = 0
+            self.total_terms_entered = 0
+
     def __init__(self):
         self.username = ""
         self.account_type = ""
         self.sign_up_date = ""
         self.profile_image = ""
-        self.statistics = QStatistics()
+        self.statistics = QUser.QStatistics()
         self.sets = []
         self.favorite_sets = []
         self.studied = []
         self.classes = []
-
-
-class QStatistics:
-    def __init__(self):
-        self.study_session_count = 0
-        self.total_answer_count = 0
-        self.public_sets_created = 0
-        self.public_terms_entered = 0
-        self.total_sets_created = 0
-        self.total_terms_entered = 0
