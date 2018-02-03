7ccb24d031dee98ad6b262db46bc40ca53532837
spikespaz <spikespaz@outlook.com>
2018-02-03 15:08:04 -0700

    Add functions to set and fetch current interface states (values)

--- application.py
+++ application.py
@@ -17,6 +17,27 @@ class Interface(Ui_batch_media_converter):
 
         self.statusbar.showMessage(status, msecs)
 
+    def fetch_state(self):
+        return {
+            "input_directory": self.input_directory_edit.text(),
+            "output_directory": self.output_directory_edit.text(),
+            "input_format": self.input_format_combo.currentText(),
+            "output_format": self.output_format_combo.currentText(),
+            "skip_present_files": self.skip_present_files_checkbox.checkState()
+        }
+
+    def set_state(self, **kwargs):
+        self.input_directory_edit.setText(kwargs.get("input_directory", ""))
+        self.output_directory_edit.setText(kwargs.get("output_directory", ""))
+
+        input_combo_items = [self.input_format_combo.itemText(item) for item in range(self.input_format_combo.count())]
+        output_combo_items = [self.output_format_combo.itemText(item) for item in range(self.output_format_combo.count())]
+
+        self.input_format_combo.setCurrentIndex(input_combo_items.index(kwargs.get("input_format")))
+        self.output_format_combo.setCurrentIndex(output_combo_items.index(kwargs.get("output_format")))
+
+        self.skip_present_files_checkbox.setChecked(kwargs.get("skip_present_files"))
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
