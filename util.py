5804ffe8c3a8264319b931d67fe69e0cbe9925dc
spikespaz <spikespaz@outlook.com>
2018-02-03 17:09:25 -0700

    Add path validator found on SO

---
+++ util.py
@@ -0,0 +1,38 @@
+import errno
+import os
+import sys
+
+ERROR_INVALID_NAME = 123
+
+
+# From https://stackoverflow.com/a/34102855
+def is_pathname_valid(pathname: str) -> bool:
+    """
+    `True` if the passed pathname is a valid pathname for the current OS;
+    `False` otherwise.
+    """
+    try:
+        if not isinstance(pathname, str) or not pathname:
+            return False
+
+        _, pathname = os.path.splitdrive(pathname)
+
+        root_dirname = os.environ.get("HOMEDRIVE", "C:") \
+            if sys.platform == "win32" else os.path.sep
+        assert os.path.isdir(root_dirname)
+
+        root_dirname = root_dirname.rstrip(os.path.sep) + os.path.sep
+
+        for pathname_part in pathname.split(os.path.sep):
+            try:
+                os.lstat(root_dirname + pathname_part)
+            except OSError as exc:
+                if hasattr(exc, "winerror"):
+                    if exc.winerror == ERROR_INVALID_NAME:
+                        return False
+                elif exc.errno in {errno.ENAMETOOLONG, errno.ERANGE}:
+                    return False
+    except TypeError as exc:
+        return False
+    else:
+        return True
