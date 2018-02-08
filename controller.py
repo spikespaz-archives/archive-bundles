fa1577fca9dfc25cd43dbb3da315f78be84fc2fb
spikespaz <spikespaz@outlook.com>
2018-02-08 16:50:00 -0700

    Rewrite FFMPEG controller and rename ffprobe wrapper

--- controller.py
+++ controller.py
@@ -1,54 +1,46 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
-
 from json import loads
-from multiprocessing import Pool
-from utilities import glob_from
-from subprocess import Popen, PIPE, check_output, DEVNULL
-from re import findall, MULTILINE
+from subprocess import Popen, check_output, DEVNULL, PIPE
 
 
-def async_convert_file(input_path, output_path, overwrite=False):
-    """Convert a file (and maybe overwrite) with FFMPEG and yield `(time, speed)` periodically."""
-    ffmpeg = Popen(("ffmpeg", "-i", input_path, output_path, "-y" if overwrite else "-n"),
-                   shell=True, stderr=PIPE)
+def run_ffmpeg(input_file, output_file, async=True, *args, **kwargs):
+    """Use FFMPEG to convert a media file."""
+    kwargs["y"] = kwargs.get("y", True)
 
-    def get_stderr():
-        return ffmpeg.stderr.read(1).decode()  # Grab one character from stderr and make it unicode
+    args = list(args)
 
-    buffer = ""  # Buffer for building matching strings
-    record = ""  # Buffer for storing the data following
-
-    time, speed = "00:00:00.00", 0.0  # The variables to be extracted
-
-    while True:  # Loop through the stderr until break
-        stderr = get_stderr()  # Each character
-
-        if stderr == "" and ffmpeg.poll() is not None:
-            break  # Stream ended end the loop
+    for arg, val in kwargs.items():
+        if isinstance(val, bool):
+            if val:
+                args.append("-" + arg)
         else:
-            buffer += stderr  # Add the character to the buffer
+            args.extend(("-" + arg, val))
 
-            if buffer.endswith("speed="):  # Match the start of the data section
-                while not record.endswith("x"):
-                    record += get_stderr()  # Add one more character until it matches the loop condition
+    ffmpeg = Popen(("ffmpeg", *args, "-progress", "-", "-i", input_file, output_file),
+                   shell=True, stderr=DEVNULL, stdout=PIPE)
 
-                speed = float(record[:-1])  # Extract the data
-            elif buffer.endswith("time="):
-                while len(record) < 11:
-                    record += get_stderr()
+    if not async:
+        return ffmpeg.wait()
+    else:
+        data = {}
 
-                time = record.replace(".", ":").split(":")  # Extract the time, in seconds
-                time = int(time[0]) * 216000 + int(time[1]) * 3600 + int(time[2]) * 60 + int(time[3])
-            else:
-                continue  # No match found yet for buffer, continue
+        while True:
+            line = ffmpeg.stdout.readline().decode().split("=")
 
-            buffer, record = "", ""  # Reset both buffers
-            yield time, speed  # Return a tuple of the extracted data of time (h, m, s, ms) and speed
+            if ffmpeg.poll() is not None:
+                break
+
+            key = line[0].strip()
+
+            if key == "progress":
+                yield data
+
+            if len(line) == 2:
+                data[key] = line[1].strip()
 
 
-def get_metadata(file_path, *args, **kwargs):
+def run_ffprobe(file_path, *args, **kwargs):
     """Use FFPROBE to get information about a media file."""
     kwargs["show_format"] = kwargs.get("show_format", True)
 
