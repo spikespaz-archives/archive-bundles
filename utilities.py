b878c34f65884086636e9f5c63d074568275be0c
spikespaz <spikespaz@outlook.com>
2018-02-10 18:44:22 -0700

    Fix error in calling "ext.lower()" in utilities

--- utilities.py
+++ utilities.py
@@ -97,7 +97,7 @@ def replace_base(input_dir, output_dir, input_path):
 
 def replace_ext(path, ext):
     """Replace the extension of a path."""
-    return os.path.splitext(path)[1] + "." + ext.lower
+    return os.path.splitext(path)[1] + "." + ext.lower()
 
 
 @contextmanager
