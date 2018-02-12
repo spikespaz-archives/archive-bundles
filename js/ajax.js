bf7d7382be7305a939f9ed63a71d4e2cd58f0a30
spikespaz <spikespaz@outlook.com>
2018-02-12 16:42:16 -0700

    Create class from ajax using es6 syntax

--- js/ajax.js
+++ js/ajax.js
@@ -1,25 +1,67 @@
-function buildQuery(params) {
-    let buffer = [];
+export class Ajax {
+    constructor(user=null, password=null) {
+        this.user = user;
+        this.password = password;
 
-    Object.keys(params).forEach(function(key) {
-        buffer.push(key + "=" + encodeURIComponent(params[key]));
-    });
+        this.requests = [];
+    }
 
-    return "?" + buffer.join("&");
-}
+    GET(url, params={}, data=null, async=true, user=this.user, password=this.password) {
+        let request = this.request("GET", params, data, async, user, password);
+        request.responseJSON = () => { return JSON.parse(request.responseText); };
 
+        this.requests.push(request);
 
-function ajaxPost(dest, params, payload, callback) {
-    let request = new XMLHttpRequest();
+        return request;
+    }
 
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                callback(request.responseText);
+    POST(url, params={}, data=null, async=true, user=this.user, password=this.password) {
+        let request = this.request("POST", params, data, async, user, password);
+        request.responseJSON = () => { return JSON.parse(request.responseText); };
+
+        this.requests.push(request);
+
+        return request;
+    }
+
+    PUT(url, params={}, data=null, async=true, user=this.user, password=this.password) {
+        let request = this.request("PUT", params, data, async, user, password);
+        request.responseJSON = () => { return JSON.parse(request.responseText); };
+
+        this.requests.push(request);
+
+        return request;
+    }
+
+    DELETE(url, params={}, data=null, async=true, user=this.user, password=this.password) {
+        let request = this.request("DELETE", params, data, async, user, password);
+        request.responseJSON = () => { return JSON.parse(request.responseText); };
+
+        this.requests.push(request);
+
+        return request;
+    }
+
+    static request(method, url, params={}, data=null, async=true, user=this.user, password=this.password) {
+        let request = new XMLHttpRequest();
+
+        request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                return request;
             }
-        }
-    };
+        };
+        
+        request.open("POST", url + this.buildQuery(params), async, user, password);
+        request.send(data);
+    }
 
-    request.open("POST", dest + buildQuery(params));
-    request.send(null);
+    static buildQuery(params) {
+        let buffer = [];
+
+        Object.keys(params).forEach(function(key) {
+            buffer.push(key + "=" + encodeURIComponent(params[key]));
+        });
+
+        return "?" + buffer.join("&");
+    }
 }
