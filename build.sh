cb3992c28dc7c34107ec8f7385c305013127525b
spikespaz <spikespaz@outlook.com>
2018-02-17 11:29:59 -0700

    Modify shell scripts to (mostly) pass shellcheck linting

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 run_sub_stage()
 {
 	log "Begin ${SUB_STAGE_DIR}"
-	pushd ${SUB_STAGE_DIR} > /dev/null
+	pushd "${SUB_STAGE_DIR}" > /dev/null
 	for i in {00..99}; do
 		if [ -f ${i}-debconf ]; then
 			log "Begin ${SUB_STAGE_DIR}/${i}-debconf"
 			on_chroot << EOF
 debconf-set-selections <<SELEOF
-`cat ${i}-debconf`
+$(cat ${i}-debconf)
 SELEOF
 EOF
 		log "End ${SUB_STAGE_DIR}/${i}-debconf"
@@ -36,16 +36,16 @@ EOF
 		fi
 		if [ -d ${i}-patches ]; then
 			log "Begin ${SUB_STAGE_DIR}/${i}-patches"
-			pushd ${STAGE_WORK_DIR} > /dev/null
+			pushd "${STAGE_WORK_DIR}" > /dev/null
 			if [ "${CLEAN}" = "1" ]; then
 				rm -rf .pc
-				rm -rf *-pc
+				rm -rf ./*-pc
 			fi
 			QUILT_PATCHES=${SUB_STAGE_DIR}/${i}-patches
-			SUB_STAGE_QUILT_PATCH_DIR="$(basename $SUB_STAGE_DIR)-pc"
-			mkdir -p $SUB_STAGE_QUILT_PATCH_DIR
-			ln -snf $SUB_STAGE_QUILT_PATCH_DIR .pc
-			if [ -e ${SUB_STAGE_DIR}/${i}-patches/EDIT ]; then
+			SUB_STAGE_QUILT_PATCH_DIR="$(basename "$SUB_STAGE_DIR")-pc"
+			mkdir -p "$SUB_STAGE_QUILT_PATCH_DIR"
+			ln -snf "$SUB_STAGE_QUILT_PATCH_DIR" .pc
+			if [ -e "${SUB_STAGE_DIR}/${i}-patches/EDIT" ]; then
 				echo "Dropping into bash to edit patches..."
 				bash
 			fi
@@ -80,20 +80,20 @@ EOF
 
 run_stage(){
 	log "Begin ${STAGE_DIR}"
-	STAGE=$(basename ${STAGE_DIR})
-	pushd ${STAGE_DIR} > /dev/null
-	unmount ${WORK_DIR}/${STAGE}
+	STAGE=$(basename "${STAGE_DIR}")
+	pushd "${STAGE_DIR}" > /dev/null
+	unmount "${WORK_DIR}/${STAGE}"
 	STAGE_WORK_DIR=${WORK_DIR}/${STAGE}
 	ROOTFS_DIR=${STAGE_WORK_DIR}/rootfs
 	if [ ! -f SKIP_IMAGES ]; then
-		if [ -f ${STAGE_DIR}/EXPORT_IMAGE ]; then
+		if [ -f "${STAGE_DIR}/EXPORT_IMAGE" ]; then
 			EXPORT_DIRS="${EXPORT_DIRS} ${STAGE_DIR}"
 		fi
 	fi
 	if [ ! -f SKIP ]; then
 		if [ "${CLEAN}" = "1" ]; then
-			if [ -d ${ROOTFS_DIR} ]; then
-				rm -rf ${ROOTFS_DIR}
+			if [ -d "${ROOTFS_DIR}" ]; then
+				rm -rf "${ROOTFS_DIR}"
 			fi
 		fi
 		if [ -x prerun.sh ]; then
@@ -102,13 +102,13 @@ run_stage(){
 			log "End ${STAGE_DIR}/prerun.sh"
 		fi
 		for SUB_STAGE_DIR in ${STAGE_DIR}/*; do
-			if [ -d ${SUB_STAGE_DIR} ] &&
-			   [ ! -f ${SUB_STAGE_DIR}/SKIP ]; then
+			if [ -d "${SUB_STAGE_DIR}" ] &&
+			   [ ! -f "${SUB_STAGE_DIR}/SKIP" ]; then
 				run_sub_stage
 			fi
 		done
 	fi
-	unmount ${WORK_DIR}/${STAGE}
+	unmount "${WORK_DIR}/${STAGE}"
 	PREV_STAGE=${STAGE}
 	PREV_STAGE_DIR=${STAGE_DIR}
 	PREV_ROOTFS_DIR=${ROOTFS_DIR}
@@ -134,11 +134,17 @@ fi
 export USE_QEMU=${USE_QEMU:-0}
 export IMG_DATE=${IMG_DATE:-"$(date +%Y-%m-%d)"}
 
-export BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export SCRIPT_DIR="${BASE_DIR}/scripts"
-export WORK_DIR=${WORK_DIR:-"${BASE_DIR}/work/${IMG_DATE}-${IMG_NAME}"}
-export DEPLOY_DIR=${DEPLOY_DIR:-"${BASE_DIR}/deploy"}
-export LOG_FILE="${WORK_DIR}/build.log"
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="${BASE_DIR}/scripts"
+WORK_DIR=${WORK_DIR:-"${BASE_DIR}/work/${IMG_DATE}-${IMG_NAME}"}
+DEPLOY_DIR=${DEPLOY_DIR:-"${BASE_DIR}/deploy"}
+LOG_FILE="${WORK_DIR}/build.log"
+
+export BASE_DIR
+export SCRIPT_DIR
+export WORK_DIR
+export DEPLOY_DIR
+export LOG_FILE
 
 export CLEAN
 export IMG_NAME
@@ -162,13 +168,13 @@ export QUILT_NO_DIFF_INDEX=1
 export QUILT_NO_DIFF_TIMESTAMPS=1
 export QUILT_REFRESH_ARGS="-p ab"
 
-source ${SCRIPT_DIR}/common
-source ${SCRIPT_DIR}/dependencies_check
+source "${SCRIPT_DIR}/common"
+source "${SCRIPT_DIR}/dependencies_check"
 
 
-dependencies_check ${BASE_DIR}/depends
+dependencies_check "${BASE_DIR}/depends"
 
-mkdir -p ${WORK_DIR}
+mkdir -p "${WORK_DIR}"
 log "Begin ${BASE_DIR}"
 
 for STAGE_DIR in ${BASE_DIR}/stage*; do
@@ -179,11 +185,11 @@ CLEAN=1
 for EXPORT_DIR in ${EXPORT_DIRS}; do
 	STAGE_DIR=${BASE_DIR}/export-image
 	source "${EXPORT_DIR}/EXPORT_IMAGE"
-	EXPORT_ROOTFS_DIR=${WORK_DIR}/$(basename ${EXPORT_DIR})/rootfs
+	EXPORT_ROOTFS_DIR=${WORK_DIR}/$(basename "${EXPORT_DIR}")/rootfs
 	run_stage
 	if [ "${USE_QEMU}" != "1" ]; then
-		if [ -e ${EXPORT_DIR}/EXPORT_NOOBS ]; then
-			source ${EXPORT_DIR}/EXPORT_NOOBS
+		if [ -e "${EXPORT_DIR}/EXPORT_NOOBS" ]; then
+			source "${EXPORT_DIR}/EXPORT_NOOBS"
 			STAGE_DIR=${BASE_DIR}/export-noobs
 			run_stage
 		fi
