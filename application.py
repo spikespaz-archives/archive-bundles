167cf6bc49559f232060ab42c916677560b4af86
Jacob Birkett <spikespaz@outlook.com>
2018-02-23 17:48:41 -0700

    Add basic live updating

--- application.py
+++ application.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from interface import Ui_MarkerWindow
+from markdown2 import Markdown
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
@@ -9,8 +10,14 @@ class MarkerWindow(Ui_MarkerWindow):
     def __init__(self, app=QApplication([]), window=QMainWindow()):
         self.window = window
         self.app = app
-
         self.setupUi(window)
+
+        self.renderer = Markdown()
+
+        self.markup_editor.textChanged.connect(self.update_preview)
+
+    def update_preview(self):
+        self.markup_preview.setText(self.renderer.convert(self.markup_editor.toPlainText()))
 
     def show(self):
         self.window.show()
