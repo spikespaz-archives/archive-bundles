676e4660e6260715f3b9fc50890186d0b3d07fd5
Jacob Birkett <spikespaz@outlook.com>
2018-02-24 18:21:14 -0700

    Add column width change to formatting in create.bat

--- create.bat
+++ create.bat
@@ -6,5 +6,5 @@ rem This doesn't use quotes, or escape unsafe file names because it assumes the 
 for %%f in (*.ui) do (
     echo %%f =^> %%~nf.py
     pyuic5 %%f -o %%~nf.py
-    yapf %%~nf.py -i
+    yapf %%~nf.py -i --style "{based_on_style: pep8, column_limit: 120}"
 )
