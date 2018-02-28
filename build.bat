080bc73a935d0edf45937aaa95c0fc881cef18b0
Jacob Birkett <spikespaz@outlook.com>
2018-02-27 20:03:49 -0700

    Add Bonjour icon and the conversion command

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 @echo off
+
+rem magick convert bonjour.png -define icon:auto-resize=16,32,48,64,128,256 bonjour.ico
+
 python -OO -m PyInstaller --ascii ^
                           --console ^
                           --onefile ^
