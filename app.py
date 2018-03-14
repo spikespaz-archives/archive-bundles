b719bdf5db2a87f9fcd11f03388f2d05d7e1f234
spikespaz <spikespaz@outlook.com>
2018-03-14 01:33:34 -0700

    Add peoperties for all list-like inputs

--- app.py
+++ app.py
@@ -16,6 +16,54 @@ class AdwinApplication:
         self.interface.window.show()
         sys.exit(self.app.exec_())
 
+    @property
+    def sources(self):
+        return common.str_to_list(self.interface.edit_sources.toPlainText())
+
+    @sources.setter
+    def sources(self, in_list):
+        self.interface.edit_sources.setPlainText(common.list_to_str(in_list))
+
+    @sources.deleter
+    def sources(self):
+        self.interface.edit_sources.clear()
+
+    @property
+    def whitelist(self):
+        return common.str_to_list(self.interface.edit_whitelist.toPlainText())
+
+    @whitelist.setter
+    def whitelist(self, in_list):
+        self.interface.edit_whitelist.setPlainText(common.list_to_str(in_list))
+
+    @whitelist.deleter
+    def whitelist(self):
+        self.interface.edit_whitelist.clear()
+
+    @property
+    def blacklist(self):
+        return common.str_to_list(self.interface.edit_blacklist.toPlainText())
+
+    @blacklist.setter
+    def blacklist(self, in_list):
+        self.interface.edit_blacklist.setPlainText(common.list_to_str(in_list))
+
+    @blacklist.deleter
+    def blacklist(self):
+        self.interface.edit_blacklist.clear()
+
+    @property
+    def redirects(self):
+        return common.str_to_list(self.interface.edit_redirects.toPlainText())
+
+    @redirects.setter
+    def redirects(self, in_list):
+        self.interface.edit_redirects.setPlainText(common.list_to_str(in_list))
+
+    @redirects.deleter
+    def redirects(self):
+        self.interface.edit_redirects.clear()
+
 
 if __name__ == "__main__":
     AdwinApplication().start()
