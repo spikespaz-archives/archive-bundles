4a37df2e2b4b56d0a43ba892273469b42348b72f
spikespaz <spikespaz@outlook.com>
2018-03-17 17:07:33 -0700

    Make str and list converters only use strings that have text

--- common.py
+++ common.py
@@ -2,8 +2,8 @@
 
 
 def str_to_list(in_str):
-    return list(map(str.strip, in_str.split("\n")))
+    return [line for line in list(map(str.strip, in_str.split("\n"))) if line]
 
 
 def list_to_str(in_list):
-    return "\n".join(in_list)
+    return "\n".join([line for line in in_list if line])
