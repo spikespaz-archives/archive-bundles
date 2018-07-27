d2fdf6fb64990113c2671e3cedb16137cafbdbf4
spikespaz <spikespaz@outlook.com>
2018-07-26 18:45:04 -0700

    Clean up example

--- example.d
+++ example.d
@@ -7,44 +7,44 @@ import std.stdio: writeln;
 /// Function to draw all implimented widgets to the NanoVega context.
 void drawWindow(SimpleWindow swnd, NVGContext nvgc) {
     // Draw the background.
-    nvgc.drawBackground(PointF(4f, 4f), SizeF(swnd.width - 8f, swnd.height - 8f));
+    nvgc.drawBackground(PointF(0, 0), SizeF(swnd.width, swnd.height));
 
     // Draw all checkbox states.
-    nvgc.drawCheckBox(PointF(14f, 14f), SizeF(14f, 14f), UNCHECKED);
-    nvgc.drawCheckBox(PointF(42f, 14f), SizeF(14f, 14f), CHECKED);
-    nvgc.drawCheckBox(PointF(70f, 14f), SizeF(14f, 14f), HOVERED);
-    nvgc.drawCheckBox(PointF(98f, 14f), SizeF(14f, 14f), ACTIVE);
+    nvgc.drawCheckBox(PointF(15, 15), SizeF(15, 15), UNCHECKED);
+    nvgc.drawCheckBox(PointF(45, 15), SizeF(15, 15), CHECKED);
+    nvgc.drawCheckBox(PointF(75, 15), SizeF(15, 15), HOVERED);
+    nvgc.drawCheckBox(PointF(105, 15), SizeF(15, 15), ACTIVE);
 
     // Draw all button states.
-    nvgc.drawButton("Button", PointF(14f, 54f), SizeF(112f, 26f));
-    nvgc.drawButton("Button", PointF(14f, 106f), SizeF(112f, 26f), CENTER_VERTICAL | HOVERED);
-    nvgc.drawButton("Button", PointF(14f, 158f), SizeF(112f, 26f), CENTER_VERTICAL | ALIGN_RIGHT | ACTIVE);
+    nvgc.drawButton("Button", PointF(15, 45), SizeF(105, 25));
+    nvgc.drawButton("HoveredButton", PointF(15, 80), SizeF(105, 25), CENTER_VERTICAL | CENTER_HORIZONTAL | HOVERED);
+    nvgc.drawButton("ActiveButton", PointF(15, 115), SizeF(105, 25), CENTER_VERTICAL | CENTER_HORIZONTAL | ACTIVE);
 
     // Draw boxes to show the alignment of text.
     nvgc.beginPath();
     nvgc.strokeColor = NVGColor.black;
     nvgc.strokeWidth = 1;
-    nvgc.rect(350f, 100, 150f, 30f);
-    nvgc.rect(500f, 100, 150f, 30f);
-    nvgc.rect(350f, 150, 150f, 30f);
-    nvgc.rect(500f, 150, 150f, 30f);
-    nvgc.rect(350f, 200, 150f, 30f);
-    nvgc.rect(500f, 200, 150f, 30f);
-    nvgc.rect(350f, 250, 150f, 30f);
-    nvgc.rect(500f, 250, 150f, 30f);
-    nvgc.rect(425f, 300, 150f, 30f);
+    nvgc.rect(swnd.width - 300 - 15, 0 + 15, 150f, 50f);
+    nvgc.rect(swnd.width - 150 - 15, 0 + 15, 150f, 50f);
+    nvgc.rect(swnd.width - 300 - 15, 50 + 15, 150f, 50f);
+    nvgc.rect(swnd.width - 150 - 15, 50 + 15, 150f, 50f);
+    nvgc.rect(swnd.width - 300 - 15, 100 + 15, 150f, 50f);
+    nvgc.rect(swnd.width - 150 - 15, 100 + 15, 150f, 50f);
+    nvgc.rect(swnd.width - 300 - 15, 150 + 15, 150f, 50f);
+    nvgc.rect(swnd.width - 150 - 15, 150 + 15, 150f, 50f);
+    nvgc.rect(swnd.width - 225 - 15, 200 + 15, 150f, 50f);
     nvgc.stroke();
 
     // Draw all alignments of text labels.
-    nvgc.drawTextLabel("Top Left", PointF(350f, 100), SizeF(150f, 30f), ALIGN_TOP | ALIGN_LEFT);
-    nvgc.drawTextLabel("Top Right", PointF(500f, 100), SizeF(150f, 30f), ALIGN_TOP | ALIGN_RIGHT);
-    nvgc.drawTextLabel("Middle Left", PointF(350f, 150), SizeF(150f, 30f), CENTER_VERTICAL | ALIGN_LEFT);
-    nvgc.drawTextLabel("Middle Right", PointF(500f, 150), SizeF(150f, 30f), CENTER_VERTICAL | ALIGN_RIGHT);
-    nvgc.drawTextLabel("Bottom Left", PointF(350f, 200), SizeF(150f, 30f), ALIGN_BOTTOM | ALIGN_LEFT);
-    nvgc.drawTextLabel("Bottom Right", PointF(500f, 200), SizeF(150f, 30f), ALIGN_BOTTOM | ALIGN_RIGHT);
-    nvgc.drawTextLabel("Top Middle", PointF(350f, 250), SizeF(150f, 30f), ALIGN_TOP | CENTER_HORIZONTAL);
-    nvgc.drawTextLabel("Bottom Middle", PointF(500f, 250), SizeF(150f, 30f), ALIGN_BOTTOM | CENTER_HORIZONTAL);
-    nvgc.drawTextLabel("Center", PointF(425f, 300), SizeF(150f, 30f), CENTER_VERTICAL | CENTER_HORIZONTAL);
+    nvgc.drawTextLabel("Top Left", PointF(swnd.width - 300 - 15, 0 + 15), SizeF(150, 50), ALIGN_TOP | ALIGN_LEFT);
+    nvgc.drawTextLabel("Top Right", PointF(swnd.width - 150 - 15, 0 + 15), SizeF(150, 50), ALIGN_TOP | ALIGN_RIGHT);
+    nvgc.drawTextLabel("Middle Left", PointF(swnd.width - 300 - 15, 50 + 15), SizeF(150, 50), CENTER_VERTICAL | ALIGN_LEFT);
+    nvgc.drawTextLabel("Middle Right", PointF(swnd.width - 150 - 15, 50 + 15), SizeF(150, 50), CENTER_VERTICAL | ALIGN_RIGHT);
+    nvgc.drawTextLabel("Bottom Left", PointF(swnd.width - 300 - 15, 100 + 15), SizeF(150, 50), ALIGN_BOTTOM | ALIGN_LEFT);
+    nvgc.drawTextLabel("Bottom Right", PointF(swnd.width - 150 - 15, 100 + 15), SizeF(150, 50), ALIGN_BOTTOM | ALIGN_RIGHT);
+    nvgc.drawTextLabel("Top Middle", PointF(swnd.width - 300 - 15, 150 + 15), SizeF(150, 50), ALIGN_TOP | CENTER_HORIZONTAL);
+    nvgc.drawTextLabel("Bottom Middle", PointF(swnd.width - 150 - 15, 150 + 15), SizeF(150, 50), ALIGN_BOTTOM | CENTER_HORIZONTAL);
+    nvgc.drawTextLabel("Center", PointF(swnd.width - 225 - 15, 200 + 15), SizeF(150, 50), CENTER_VERTICAL | CENTER_HORIZONTAL);
 
 }
 
