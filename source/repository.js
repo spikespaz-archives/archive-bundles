1b8adec2926dc62f7e6bd54eb70cbe399363566e
spikespaz <spikespaz@outlook.com>
2018-09-26 23:50:40 -0700

    Update dl button code to use api creds

--- source/repository.js
+++ source/repository.js
@@ -1,5 +1,4 @@
-
-const github = new Octokat();
+let github;
 
 function addDlButton() {
     let urlMatch = window.location.pathname.match(/\/([\w-]+)\/([\w-]+)/);
@@ -29,10 +28,10 @@ function createDlButton(btnUrl, numUrl, count) {
     buttonEl.id = "downloads-social-button";
     buttonEl.innerHTML = `
     <a class="btn btn-sm btn-with-count" href="${btnUrl}" title="Go to the latest release version">
-        <svg width='16' height='16' class='octicon octicon-desktop-download v-align-text-bottom' viewBox='0 0 16 16' version='1.1' aria-hidden='true'>
-            <path fill-rule='evenodd' d='M4 6h3V0h2v6h3l-4 4-4-4zm11-4h-4v1h4v8H1V3h4V2H1c-.55 0-1 .45-1 1v9c0 .55.45 1 1 1h5.34c-.25.61-.86 1.39-2.34 2h8c-1.48-.61-2.09-1.39-2.34-2H15c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1z'></path>
-        </svg>
-        Download
+    <svg width='16' height='16' class='octicon octicon-desktop-download v-align-text-bottom' viewBox='0 0 16 16' version='1.1' aria-hidden='true'>
+    <path fill-rule='evenodd' d='M4 6h3V0h2v6h3l-4 4-4-4zm11-4h-4v1h4v8H1V3h4V2H1c-.55 0-1 .45-1 1v9c0 .55.45 1 1 1h5.34c-.25.61-.86 1.39-2.34 2h8c-1.48-.61-2.09-1.39-2.34-2H15c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1z'></path>
+    </svg>
+    Download
     </a>
     <a class="social-count" href="${numUrl}">${count}</a>
     `;
@@ -40,4 +39,8 @@ function createDlButton(btnUrl, numUrl, count) {
     return buttonEl;
 }
 
-addDlButton();
+browser.storage.local.get().then(result => {
+    github = new Octokat(result);
+
+    addDlButton();
+});
