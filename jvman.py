bf04dc21caffaa1d6bc78489acfba04aaac54709
Jacob Birkett <spikespaz@outlook.com>
2019-03-06 21:57:55 -0700

    Add class for finding default request params

--- jvman.py
+++ jvman.py
@@ -1,10 +1,43 @@
 import adoptapi
 import click
 import json
+import platform
+
+
+def _replacement(value, substitutes):
+    return substitutes.get(value, value)
+
+
+class RequestOptions:
+    def __init__(self, **kwargs):
+        self.openjdk_impl = kwargs.get("openjdk_impl", None)
+        self.os = kwargs.get("os", _replacement(platform.system().lower(), {"darwin": "mac"}))
+        self.arch = kwargs.get(
+            "arch",
+            _replacement(
+                platform.machine().lower(),
+                {
+                    "amd64": "x64",
+                    "x86_64": "x64",
+                    "aarch64_be": "aarch64",
+                    "armv8b": "aarch64",
+                    "armv8l": "aarch64",
+                    "i386": "x32",
+                    "i686": "x32",
+                    "s390": "s390x",
+                },
+            ),
+        )
+        self.release = kwargs.get("release", None)
+        self.type = kwargs.get("type", None)
+        self.heap_size = kwargs.get("heap_size", "normal")
+
+    def serialize(self):
+        return self.__dict__
 
 
 @click.group()
-@click.option("-v", "--version", help="Display the version number and exit.")
+@click.option("-v", "--version", is_flag=True, help="Display the version number and exit.")
 def cli(version):
     pass
 
@@ -12,7 +45,7 @@ def cli(version):
 @cli.command()
 @click.argument("version", type=str, default="openjdk8", required=True)
 @click.argument("out_file", type=click.File(mode="w"), default="-")
-@click.option("-n", "--nightly", type=bool, default=False)
+@click.option("-n", "--nightly", is_flag=True, default=False)
 @click.option("-i", "--indent", type=int, default=None)
 @click.option("--openjdk-impl", type=click.Choice(["hotspot", "openj9"]))
 @click.option("--os", type=click.Choice(["windows", "linux", "mac"]))
@@ -28,7 +61,12 @@ def info(version, out_file, nightly, indent, **kwargs):
             map(adoptapi.Release.serialize, adoptapi.info(version, nightly=nightly, **kwargs))
         )
 
-    json.dump(dump_data, out_file, indent=indent, separators=(", ", ": ") if indent else (",", ":"))
+    json.dump(
+        dump_data,
+        out_file,
+        indent=indent,
+        separators=(",", ":") if indent is None else (", ", ": "),
+    )
 
 
 @cli.command()
