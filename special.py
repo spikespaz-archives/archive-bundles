06a55180f21f436cf78d8d536b36a4e4fe66b287
spikespaz <spikespaz@outlook.com>
2019-03-11 01:05:06 -0700

    Add subclass of QAbstractTableModel for specific API info

--- special.py
+++ special.py
@@ -1,7 +1,11 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt, QModelIndex, QVariant, QAbstractTableModel
+from PyQt5.QtWidgets import QButtonGroup
+from adoptapi import Release, ReleaseAsset
+
+import copy
 
 
-class CheckBoxButtonGroup(QtWidgets.QButtonGroup):
+class CheckBoxButtonGroup(QButtonGroup):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -24,3 +28,86 @@ class CheckBoxButtonGroup(QtWidgets.QButtonGroup):
 
     def checked_buttons(self):
         return [button for button in self.buttons() if button.isChecked()]
+
+
+class AvailableBinariesTableModel(QAbstractTableModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._column_names = [
+            "Release Name",
+            "Java Version",
+            "Release Type",
+            "Binary Type",
+            "Virtial Machine",
+            "Heap Size",
+            "Architecture",
+        ]
+        self._internal_data = []
+
+    def rowCount(self, parent=QModelIndex()):
+        return len(self._internal_data)
+
+    def columnCount(self, parent=QModelIndex()):
+        return len(self._column_names)
+
+    def data(self, index, role=Qt.DisplayRole):
+        if (
+            not index.isValid()
+            or index.row() > self.rowCount()
+            or index.column() > self.columnCount()
+        ):
+            return QVariant()
+
+        if role == Qt.DisplayRole:
+            release = self._internal_data[index.row()]
+
+            if index.column() == 0:  # Release Name
+                return release.release_name
+            elif index.column() == 1:  # Java Version
+                return release.binaries[0].version
+            elif index.column() == 2:  # Release Type
+                return "Release" if release.release else "Nightly"
+            elif index.column() == 3:  # Binary Type
+                return release.binaries[0].binary_type.upper()
+            elif index.column() == 4:  # Virtual Machine
+                if release.binaries[0].openjdk_impl == "hotspot":
+                    return "Oracle HotSpot"
+                elif release.binaries[0].openjdk_impl == "openj9":
+                    return "Eclipse OpenJ9"
+                else:
+                    return release.openjdk_impl
+            elif index.column() == 5:  # Heap Size
+                return release.binaries[0].heap_size.title()
+            elif index.column() == 6:  # Architecture
+                return release.binaries[0].architecture
+
+        return QVariant()
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if role != Qt.DisplayRole:
+            return QVariant()
+
+        if orientation == Qt.Horizontal:
+            return self._column_names[section]
+        else:
+            return section
+
+        return QVariant()
+
+    def insertRows(self, position, rows, parent):
+        self.beginInsertRows(QModelIndex(), position, position + rows - 1)
+
+        for row in range(rows):
+            self._internal_data.insert(Release(binaries=[ReleaseAsset()]))
+
+        self.endInsertRows()
+
+        return True
+
+    def add_release(self, release):
+        for binary in release.binaries:
+            standalone = copy.copy(release)
+            standalone.binaries = [copy.copy(binary)]
+
+            self._internal_data.append(standalone)
