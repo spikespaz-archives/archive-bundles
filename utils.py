0aa73c3df86f26fbd4565cac539ab8662400b0c5
Jacob Birkett <spikespaz@outlook.com>
2019-03-13 14:43:36 -0700

    Add subclass for a QThread for function decoration

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import itertools
 
+from PyQt5.QtCore import QThread
+
 
 def wrap_throwable(func, *exc):
     def wrapper(*args, **kwargs):
@@ -17,3 +19,27 @@ def product_dicts(**kwargs):
 
     for instance in itertools.product(*values):
         yield dict(zip(keys, instance))
+
+
+class BackgroundThread(QThread):
+    def __init__(self, destination, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        destination.append(self)
+
+    def __call__(self, function):
+        self._target = function
+
+        def wrapper(*args, **kwargs):
+            self._args = args
+            self._kwargs = kwargs
+
+            self.start()
+
+        return wrapper
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        self._target(*self._args, **self._kwargs)
