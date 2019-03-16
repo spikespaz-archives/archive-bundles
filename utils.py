44f0328ac77c76d3027c1a840a66344f1680af4d
Jacob Birkett <spikespaz@outlook.com>
2019-03-16 02:08:38 -0700

    Add thread superclass for downloading files

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import itertools
+import requests
+import re
 
 from PyQt5.QtCore import QThread
+from PyQt5 import QtCore
+from os import path
 
 
 def wrap_throwable(func, *exc):
@@ -43,3 +47,43 @@ class BackgroundThread(QThread):
 
     def run(self):
         self._target(*self._args, **self._kwargs)
+
+
+class DownloaderThread(QThread):
+    filenameFound = QtCore.pyqtSignal(str)
+    filesizeFound = QtCore.pyqtSignal(int)
+    bytesChanged = QtCore.pyqtSignal(int)
+
+    def __init__(self, chunk_size=512, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.chunk_size = chunk_size
+        self.filename = None
+        self._url = None
+        self._location = None
+
+    def __del__(self):
+        self.wait()
+
+    def __call__(self, url, location="./"):
+        self._url = url
+        self._location = location
+
+        self.start()
+
+    def run(self):
+        request = requests.get(self._url)
+        filesize = int(request.headers["content-length"])
+        self.filename = re.findall("filename=(.+)", request.headers["content-disposition"])[0]
+
+        self.filenameFound.emit(self.filename)
+        self.filesizeFound.emit(filesize)
+
+        with open(path.join(self._location, self.filename), "wb") as file:
+            for count, chunk in enumerate(request.iter_content(chunk_size=self.chunk_size), 1):
+                if not chunk:
+                    continue
+
+                file.write(chunk)
+
+                self.bytesChanged.emit(count * self.chunk_size)
