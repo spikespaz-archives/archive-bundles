e30c5cadadd16d0bd0cc8e64bc8f9c371639ab61
Jacob Birkett <spikespaz@outlook.com>
2019-03-24 01:56:55 -0700

    Move GUI code to submodule and subcommand

--- jvman/__main__.py
+++ jvman/__main__.py
@@ -1,10 +1,14 @@
-from main import *
+import sys
 
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = QMainWindow()
-    ui = AppMainWindow()
-    ui.setupUi(window)
-    window.show()
-    sys.exit(app.exec_())
+    if "gui" in sys.argv:
+        from gui import AppMainWindow
+        from PyQt5.QtWidgets import QApplication, QMainWindow
+
+        app = QApplication([])
+        window = QMainWindow()
+        ui = AppMainWindow()
+        ui.setupUi(window)
+        window.show()
+        sys.exit(app.exec_())
