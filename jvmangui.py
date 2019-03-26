1632238adf8f88e804a18421e378b1bbf58a1315
spikespaz <spikespaz@outlook.com>
2019-03-26 06:05:23 -0700

    Set fusion as default theme for GUI

--- jvmangui.py
+++ jvmangui.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 if __name__ == "__main__":
     app = QApplication([])
+    app.setStyle("fusion")
     window = QMainWindow()
     ui = AppMainWindow()
     ui.setupUi(window)
