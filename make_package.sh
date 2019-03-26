e2e302cf7a7c84446ed80c4cac87689f802e0d66
spikespaz <spikespaz@outlook.com>
2019-03-25 17:19:56 -0700

    Add Nuitka compile command

--- make_package.sh
+++ make_package.sh
@@ -4,4 +4,9 @@ if !(which python3 &> /dev/null); then
     alias python3=python
 fi
 
-python3 -OO -m PyInstaller --noconfirm --noupx --windowed jvmangui.py
+python3 -OO -m nuitka --standalone \
+                      --follow-imports \
+                      --plugin-enable=qt-plugins \
+                      jvmangui.py
+
+chmod +x "./jvmangui.dist/jvmangui"
