e1e759beff8156bb882ca5c3e011a777fbc96bd1
spikespaz <spikespaz@outlook.com>
2019-03-26 06:05:50 -0700

    Revise shell scripts, again

--- make_package.sh
+++ make_package.sh
@@ -1,12 +1,7 @@
-rm -rf build dist
-
-if !(which python3 &> /dev/null); then
-    alias python3=python
-fi
-
-python3 -OO -m nuitka --standalone \
-                      --follow-imports \
-                      --plugin-enable=qt-plugins \
-                      jvmangui.py
+python -OO -m nuitka --standalone \
+                     --follow-imports \
+                     --plugin-enable=qt-plugins \
+                     --experimental=use_pefile \
+                     "jvmangui.py"
 
 chmod +x "./jvmangui.dist/jvmangui"
