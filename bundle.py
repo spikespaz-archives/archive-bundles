b0a03f55ac98777ebd29f42427baf6c0ca780ae1
Jacob Birkett <spikespaz@outlook.com>
2019-05-31 02:19:43 -0700

    Add script to automate archival process

---
+++ bundle.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+
+## THIS IS JUST SOME LAZY CODE TO GET THE JOB DONE
+## PLEASE DON'T HURT ME
+
+from os import path, listdir, chdir, getcwd, rename, makedirs
+from subprocess import call
+from re import match
+
+GIT_URL_RE = r"https?:\/\/([\w-.]+)\/([\w-]+)\/([\w-]+)"
+CWD = getcwd()
+
+# (url, author, repo)
+repo_list = []
+
+with open("repo_list.txt") as fp:
+    for line in fp.readlines():
+        line = line.strip()
+
+        if not line:
+            continue
+
+        m = match(GIT_URL_RE, line)
+
+        # Add a new tuple and make a new url that's guaranteed
+        repo_list.append(("https://{}/{}/{}".format(*m.groups()), m.groups()[1], m.groups()[2]))
+
+
+for repo in repo_list:
+    call("git clone {} {}.{}".format(*repo))
+
+makedirs("bundles", exist_ok=True)
+
+for item in listdir():
+    if path.isfile(item) or item == "bundles":
+        continue
+
+    # Enter repo dir
+    chdir(item)
+
+    # bundle and move the resulting file
+    call("git bundle create self.bundle --all")
+    rename("self.bundle", path.join(CWD, "bundles", item + ".bundle"))
+
+    # exit repo
+    chdir(CWD)
