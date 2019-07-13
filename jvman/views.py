60ac67b7d8084a17c100449e63c9f4deafa9a9c8
Jacob Birkett <spikespaz@outlook.com>
2019-07-13 01:16:05 -0700

    Add double-click to copy to clipboard in details view

--- jvman/views.py
+++ jvman/views.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QDialog, QTreeView, QVBoxLayout, QAbstractItemView
 from PyQt5.QtCore import Qt
+from PyQt5.Qt import QClipboard, QApplication
 
 from .models import BinaryDetailsTreeModel
 
@@ -31,6 +32,11 @@ class BinaryDetailsDialog(QDialog):
         self.details_tree_view.setModel(self.details_tree_model)
         self.details_tree_view.expandAll()
         self.details_tree_view.resizeColumnToContents(0)
+        self.details_tree_view.doubleClicked.connect(
+            lambda index: QApplication.clipboard().setText(
+                self.details_tree_model.data(index, Qt.DisplayRole)
+            )
+        )
 
         self.layout.addWidget(self.details_tree_view)
         self.setLayout(self.layout)
