c62bdc6fbb53928bafb1158f559b6cef5a56b1a7
Jacob Birkett <spikespaz@outlook.com>
2019-07-14 01:05:23 -0700

    Add copy for ui file after nuitka compile

--- make_package.sh
+++ make_package.sh
@@ -2,6 +2,8 @@ python -OO -m nuitka --standalone \
                      --follow-imports \
                      --plugin-enable=qt-plugins \
                      --experimental=use_pefile \
-                     "jvmangui.py"
+                     "./jvmangui.py"
 
 chmod +x "./jvmangui.dist/jvmangui"
+mkdir -p "./jvmangui.dist/jvman/"
+cp "./jvman/interface.ui" "./jvmangui.dist/jvman/"
