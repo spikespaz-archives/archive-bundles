9aad2fd1353c10528a564204de5e2edd7e0e6c1d
Jacob Birkett <spikespaz@outlook.com>
2019-07-14 16:42:44 -0700

    Add theme loader for nuitka build

---
+++ jvtgui.py
@@ -0,0 +1,88 @@
+import sys
+
+from jvtgui.gui import AppMainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtCore import Qt
+
+
+if __name__ == "__main__":
+    APP = QApplication([])
+    WINDOW = AppMainWindow()
+
+    print(sys.argv)
+    
+    if "fusion" in sys.argv:
+        APP.setStyle("fusion")
+
+    if "fusion-dark" in sys.argv:
+        PALETTE = QPalette()
+
+        # Color palette and stylesheet adapted from the gist below.
+        # https://gist.github.com/lschmierer/443b8e21ad93e2a2d7eb
+
+        PALETTE.setColor(QPalette.Window, QColor(53, 53, 53))
+        PALETTE.setColor(QPalette.WindowText, Qt.white)
+        PALETTE.setColor(QPalette.Base, QColor(45, 45, 45))
+        PALETTE.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+        PALETTE.setColor(QPalette.ToolTipBase, Qt.white)
+        PALETTE.setColor(QPalette.ToolTipText, Qt.white)
+        PALETTE.setColor(QPalette.Text, Qt.white)
+        PALETTE.setColor(QPalette.Button, QColor(53, 53, 53))
+        PALETTE.setColor(QPalette.ButtonText, Qt.white)
+        PALETTE.setColor(QPalette.BrightText, Qt.red)
+        PALETTE.setColor(QPalette.HighlightedText, Qt.black)
+
+        if "accent-orange" in sys.argv:
+            PALETTE.setColor(QPalette.Link, QColor(218, 115, 42))
+            PALETTE.setColor(QPalette.Highlight, QColor(218, 115, 42))
+
+            APP.setStyleSheet(
+                """
+                QToolTip {
+                    color: #ffffff;
+                    background-color: #2a82da;
+                    border: 1px solid white;
+                }
+                """
+            )
+        else:
+            PALETTE.setColor(QPalette.Link, QColor(42, 130, 218))
+            PALETTE.setColor(QPalette.Highlight, QColor(42, 130, 218))
+
+            APP.setStyleSheet(
+                """
+                QToolTip {
+                    color: #ffffff;
+                    background-color: #da732a;
+                    border: 1px solid white;
+                }
+                """
+            )
+       
+        APP.setPalette(PALETTE)
+
+     
+
+    if "accent-orange" in sys.argv:
+        PALETTE = QPalette()
+
+        # Color palette and stylesheet adapted from the gist below.
+        # https://gist.github.com/lschmierer/443b8e21ad93e2a2d7eb
+
+        
+
+        APP.setPalette(PALETTE)
+
+        APP.setStyleSheet(
+            """
+            QToolTip {
+                color: #ffffff;
+                background-color: #da732a;
+                border: 1px solid white;
+            }
+            """
+        )
+
+    WINDOW.show()
+    sys.exit(APP.exec_())
--- make_package.sh
+++ make_package.sh
@@ -2,8 +2,50 @@ python -OO -m nuitka --standalone \
                      --follow-imports \
                      --plugin-enable=qt-plugins \
                      --experimental=use_pefile \
-                     "./jvmangui.py"
+                     --windows-disable-console \
+                     "./jvtgui.py"
 
-chmod +x "./jvmangui.dist/jvmangui"
-mkdir -p "./jvmangui.dist/jvman/"
-cp "./jvman/interface.ui" "./jvmangui.dist/jvman/"
+cat > "./jvtgui.dist/run_jvtgui.bat" << EOL
+@echo off
+
+set PATH=%PATH%;%~dp0
+
+rem NOTE: If you wish, you may use variants of the Fusion theme by changing the arguments.
+rem For the default Qt Fusion theme: "fusion"
+rem For a custom Fusion Dark theme with a blue accent: "fusion fusion-dark"
+rem With an orange accent (this is for you brother): "fusion fusion-dark orange-accent"
+rem Or just nothing to let the gods decide.
+
+set ARGS=fusion fusion-dark accent-orange
+
+start "" jvtgui.exe %ARGS%
+
+exit
+EOL
+
+cat > "./jvtgui.dist/run_jvtgui.sh" << EOL
+#! /usr/bin/sh
+export PATH="\$PATH:\${pwd}"
+
+# NOTE: If you wish, you may use variants of the Fusion theme.
+
+# For the default Qt Fusion theme:
+# ./jvtgui fusion
+
+# For a custom Fusion Dark theme with a blue accent:
+# ./jvtgui fusion fusion-dark
+
+For a custom Fusion Dark theme with an orange accent (this is for you brother):
+# ./jvtgui fusion fusion-dark accent-orange
+
+# Let the gods decide
+./jvtgui
+
+exit
+EOL
+
+mkdir -p "./jvtgui.dist/jvtgui/"
+cp "./jvtgui/interface.ui" "./jvtgui.dist/jvtgui/"
+
+chmod +x "./jvtgui.dist/jvtgui"
+chmod +x "./jvtgui.dist/run_jvtgui.sh"
