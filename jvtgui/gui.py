a50845faa4ee883193074aac677ac068c2e3132f
Jacob Birkett <spikespaz@outlook.com>
2019-07-26 11:55:40 -0700

    Add OpenJDK 12 filter

--- jvtgui/gui.py
+++ jvtgui/gui.py
@@ -170,7 +170,7 @@ class AppMainWindow(QMainWindow):
             self.javaVer9CheckBox,
             self.javaVer10CheckBox,
             self.javaVer11CheckBox,
-            self.javaVer8CheckBox,
+            self.javaVer12CheckBox,
         )
 
         self.releaseTypeButtonGroup = CheckBoxButtonGroup(self)
@@ -581,6 +581,9 @@ class AppMainWindow(QMainWindow):
         if self.javaVer11CheckBox.isChecked():
             options._version.append("openjdk11")
 
+        if self.javaVer12CheckBox.isChecked():
+            options._version.append("openjdk12")
+
         if self.stableReleaseTypeCheckBox.isChecked():
             options._nightly.append(False)
 
@@ -622,6 +625,7 @@ class AppMainWindow(QMainWindow):
         self.javaVer9CheckBox.setChecked("openjdk9" in options._version)
         self.javaVer10CheckBox.setChecked("openjdk10" in options._version)
         self.javaVer11CheckBox.setChecked("openjdk11" in options._version)
+        self.javaVer11CheckBox.setChecked("openjdk12" in options._version)
 
         self.stableReleaseTypeCheckBox.setChecked(False in options._nightly)
         self.nightlyReleaseTypeCheckBox.setChecked(True in options._nightly)
