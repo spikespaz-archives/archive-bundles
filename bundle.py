f22aa6234251e5118d829d33b9106f802f818f79
Jacob Birkett <spikespaz@outlook.com>
2019-08-07 16:47:32 -0700

    Make script workfrom cwd

--- bundle.py
+++ bundle.py
@@ -3,7 +3,7 @@
 ## THIS IS JUST SOME LAZY CODE TO GET THE JOB DONE
 ## PLEASE DON'T HURT ME
 
-from os import path, listdir, chdir, getcwd, rename, makedirs
+from os import path, listdir, chdir, getcwd, rename, remove, removedirs
 from subprocess import call
 from re import match
 
@@ -32,18 +32,29 @@ with open("repo_list.txt") as fp:
 for repo in repo_list:
     call("git clone {} {}.{}".format(*repo))
 
-makedirs("bundles", exist_ok=True)
-
 for item in listdir():
-    if path.isfile(item) or item == "bundles":
+    if not path.isdir(item) or item in (".git",):
         continue
 
     # Enter repo dir
     chdir(item)
 
+    bundle_file = path.join(CWD, item + '.bundle')
+
+    print(f"Creating bundle for '{bundle_file}'")
+
     # bundle and move the resulting file
     call("git bundle create self.bundle --all")
-    rename("self.bundle", path.join(CWD, "bundles", item + ".bundle"))
+
+    if path.isfile(bundle_file):
+        print("Deleting existing bundle file...")
+
+        remove(bundle_file)
+
+    rename("self.bundle", path.join(CWD, item + ".bundle"))
+
+    print("Deleting clone directory...")
+    removedirs(item)
 
     # exit repo
     chdir(CWD)
