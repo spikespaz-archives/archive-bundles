2be3c00f35536923be959c7ff1d193075caa1fde
BuildTools <spikespaz@outlook.com>
2019-10-12 19:34:19 -0700

    Rewrite InventoryClickEvent listener

--- src/main/java/com/spikeapaz/spigot/deconstructiontable/PluginInventoryHolder.java
+++ src/main/java/com/spikeapaz/spigot/deconstructiontable/PluginInventoryHolder.java
@@ -4,9 +4,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -92,82 +94,103 @@ class PluginInventoryHolder implements InventoryHolder {
         inventory.setItem(11, item);
     }
 
+    // Delegate for the event handler that has access to an instance of this class.
+    // This is here so that only one event handler for clicks must exist, the rest is just a method here.
     void handleClick(InventoryClickEvent event) {
-        if (event.getClickedInventory() == null)
-            return;
-
-        final ItemStack item = event.getCurrentItem();
-        if (item == null) return;
-        final ItemMeta meta = item.getItemMeta();
-        if (meta == null) return;
-
         if (emptySlots.contains(event.getRawSlot())) {
             event.setCancelled(true);
             return;
         }
 
-        final ItemStack cursor = event.getCursor();
-
-        Bukkit.broadcastMessage("Click Event!");
-
-        if (cursor == null) {
-            Bukkit.broadcastMessage("Cursor is null.");
-            return;
+        switch (event.getAction()) {
+            case NOTHING:
+            case MOVE_TO_OTHER_INVENTORY:
+                return;
+            case COLLECT_TO_CURSOR:
+            case PICKUP_ALL:
+                Bukkit.broadcastMessage("Item has been removed.");
+                break;
+            case DROP_ALL_CURSOR:
+            case DROP_ALL_SLOT:
+            case DROP_ONE_CURSOR:
+            case DROP_ONE_SLOT:
+            case PLACE_ALL:
+            case PLACE_ONE:
+            case PLACE_SOME:
+                if (event.getRawSlot() == 11) {
+                    Bukkit.broadcastMessage("Item has been inserted.");
+                    return;
+                } else
+                    event.setCancelled(true);
+                break;
+            case SWAP_WITH_CURSOR:
+                if (event.getRawSlot() == 11) {
+                    Bukkit.broadcastMessage("Swapped with cursor.");
+                    return;
+                } else
+                    event.setCancelled(true);
+                break;
+            case UNKNOWN:
+                Utils.tellConsole("Unknown inventory action.");
+                break;
+            default:
+                Bukkit.broadcastMessage(event.getAction().toString());
+                event.setCancelled(true);
         }
-        else if (cursor.getType().equals(Material.AIR) && !item.getType().equals(Material.AIR)) {
-            // Player has put an item in the inventory
-            Bukkit.broadcastMessage("Player has put an item in the inventory.");
-        }
-        else if (cursor.getType().equals(Material.AIR) && !item.getType().equals(Material.AIR)) {
-            // Player has taken an item
-            Bukkit.broadcastMessage("Player has taken an item.");
-        }
-        else if (!cursor.getType().equals(Material.AIR) && !item.getType().equals(Material.AIR)) {
-            // Player has swapped an item for another
-            Bukkit.broadcastMessage("Player has swapped an item for another.");
-        }
-
-        Bukkit.broadcastMessage("Cursor: " + cursor.getType().toString());
-        Bukkit.broadcastMessage("Slot: " + item.getType().toString());
     }
 
+    // Delegate for the event handler that has access to an instance of this class.
+    // This is here so that only one event handler for drags must exist, the rest is just a method here.
     void handleDrag(InventoryDragEvent event) {
-        final ArrayList<ItemStack> items = new ArrayList<>(event.getNewItems().values());
+//        final ArrayList<ItemStack> items = new ArrayList<>(event.getNewItems().values());
+//
+//        boolean blankChanged = false;
+//        for (int slot : event.getRawSlots()) {
+//            blankChanged = (slot >= 1 && slot <= 4) || (slot >= 9 && slot <= 12) || blankChanged;
+//        }
+//
+//        if (blankChanged) {
+//            event.setCancelled(true);
+//            return;
+//        }
+//
+//        final ItemStack cursor = event.getCursor();
+//
+//        Bukkit.broadcastMessage("Drag Event!");
+//
+//        if (cursor == null) {
+//            Bukkit.broadcastMessage("Cursor is null.");
+//            return;
+//        }
+//        else if (cursor.getType().equals(Material.AIR) && items.size() > 0) {
+//            // Player has put an item in the inventory
+//            Bukkit.broadcastMessage("Player has put an item in the inventory.");
+//        }
+//        else if (cursor.getType().equals(Material.AIR) && items.size() == 0) {
+//            // Player has taken an item
+//            Bukkit.broadcastMessage("Player has taken an item.");
+//        }
+//        else if (!cursor.getType().equals(Material.AIR) && items.size() > 0) {
+//            // Player has swapped an item for another
+//            Bukkit.broadcastMessage("Player has swapped an item for another.");
+//        }
+//
+//        Bukkit.broadcastMessage("Cursor: " + cursor.getType().toString());
+//        ArrayList<String> slottedItems = items.stream().map(item -> item.getItemMeta().getDisplayName()).collect(Collectors.toCollection(ArrayList::new));
+//        Bukkit.broadcastMessage("Slot: " + String.join(", ", slottedItems));
+    }
 
-        boolean blankChanged = false;
-        for (int slot : event.getRawSlots()) {
-            blankChanged = (slot >= 1 && slot <= 4) || (slot >= 9 && slot <= 12) || blankChanged;
-        }
+    // Delegate for the event handler that has access to an instance of this class.
+    // This is here so that only one event handler for moves must exist, the rest is just a method here.
+    void handleMove(InventoryMoveItemEvent event) {
+        InventoryHolder baseSource = event.getSource().getHolder();
+        InventoryHolder baseDestination = event.getDestination().getHolder();
+        if (baseSource == null || baseDestination == null) return;
 
-        if (blankChanged) {
-            event.setCancelled(true);
-            return;
-        }
-
-        final ItemStack cursor = event.getCursor();
-
-        Bukkit.broadcastMessage("Drag Event!");
-
-        if (cursor == null) {
-            Bukkit.broadcastMessage("Cursor is null.");
-            return;
-        }
-        else if (cursor.getType().equals(Material.AIR) && items.size() > 0) {
-            // Player has put an item in the inventory
-            Bukkit.broadcastMessage("Player has put an item in the inventory.");
-        }
-        else if (cursor.getType().equals(Material.AIR) && items.size() == 0) {
-            // Player has taken an item
-            Bukkit.broadcastMessage("Player has taken an item.");
-        }
-        else if (!cursor.getType().equals(Material.AIR) && items.size() > 0) {
-            // Player has swapped an item for another
-            Bukkit.broadcastMessage("Player has swapped an item for another.");
-        }
-
-        Bukkit.broadcastMessage("Cursor: " + cursor.getType().toString());
-        ArrayList<String> slottedItems = items.stream().map(item -> item.getItemMeta().getDisplayName()).collect(Collectors.toCollection(ArrayList::new));
-        Bukkit.broadcastMessage("Slot: " + String.join(", ", slottedItems));
+//        if (PluginInventoryHolder.class.isAssignableFrom(baseSource.getClass()))
+//            return;
+//        else if (PluginInventoryHolder.class.isAssignableFrom(baseDestination.getClass()))
+//            if (event.getItem())
     }
 
     @Override
