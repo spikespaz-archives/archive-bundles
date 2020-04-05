644d3b4e292dca2ccd92a1cb5fcaee07624159d4
BuildTools <spikespaz@outlook.com>
2020-04-05 14:54:06 -0700

    Fix isMouseOver and button highlighting

---
+++ src/main/java/com/spikespaz/radialmenu/MathHelper.java
@@ -0,0 +1,29 @@
+package com.spikespaz.radialmenu;
+
+public class MathHelper {
+    public static final double TWO_PI = Math.PI * 2;
+
+    public static double normAngle(double angle) {
+        return normAngle(angle, TWO_PI);
+    }
+
+    public static double normAngle(double angle, double range) {
+        return (angle % range + range) % range;
+    }
+
+    public static double angleDiff(double a0, double a1) {
+        double r = (a0 - a1) % TWO_PI;
+        if (r < -Math.PI)
+            r += TWO_PI;
+        if (r >= Math.PI)
+            r -= TWO_PI;
+        return r;
+    }
+
+    public static boolean angleBetween(double start, double end, double mid) {
+        start = normAngle(start);
+        end = normAngle(normAngle(end) - start);
+        mid = normAngle(normAngle(mid) - start);
+        return (mid < end);
+    }
+}
--- src/main/java/com/spikespaz/radialmenu/gui/GuiRadialButton.java
+++ src/main/java/com/spikespaz/radialmenu/gui/GuiRadialButton.java
@@ -1,5 +1,6 @@
 package com.spikespaz.radialmenu.gui;
 
+import com.spikespaz.radialmenu.MathHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.ScaledResolution;
@@ -16,15 +17,10 @@ public class GuiRadialButton extends GuiButton {
     public int hoverColor;
 
     public GuiRadialButton(int buttonId, int radius, int deadRadius, int thickness, int sliceCount, int sliceNum, int color, int hoverColor) {
-        super(buttonId, 0, 0, "");
-        this.id = buttonId;
-        this.radius = radius;
-        this.deadRadius = deadRadius;
+        this(buttonId, radius, deadRadius, thickness, color, hoverColor);
         this.thickness = thickness;
         this.sliceCount = sliceCount;
         this.sliceNum = sliceNum;
-        this.color = color;
-        this.hoverColor = hoverColor;
     }
 
     public GuiRadialButton(int buttonId, int radius, int deadRadius, int thickness, int color, int hoverColor) {
@@ -42,37 +38,29 @@ public class GuiRadialButton extends GuiButton {
     public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks) {
         if (!this.visible) return;
 
-        this.hovered = this.isMouseOver(mouseX, mouseY);
+        this.hovered = this.isMouseOver(mc, mouseX, mouseY);
 
         final ScaledResolution scaledRes = new ScaledResolution(mc);
-
-        this.sliceCount = 7;
 
         final double cx = scaledRes.getScaledWidth_double() / 2;
         final double cy = scaledRes.getScaledHeight_double() / 2;
 
         final double sa = Math.PI * 2 / this.sliceCount; // Slice angle
-        final double ssa = this.sliceNum * sa - sa / 2; // Start slice angle
+        final double ssa = (this.sliceCount - this.sliceNum) * sa - sa / 2 + Math.PI; // Start slice angle
         final double esa = ssa + sa; // End slice angle
         final double ipr = this.radius / Math.cos(sa / 2); // Inner point radius
         final double opr = ipr + this.thickness; // Outer point radius
 
-        final double x3 = cx + Math.sin(ssa) * ipr;
-        final double y3 = cy - Math.cos(ssa) * ipr;
-        final double x2 = cx + Math.sin(ssa) * opr;
-        final double y2 = cy - Math.cos(ssa) * opr;
-        final double x1 = cx + Math.sin(esa) * opr;
-        final double y1 = cy - Math.cos(esa) * opr;
-        final double x0 = cx + Math.sin(esa) * ipr;
-        final double y0 = cy - Math.cos(esa) * ipr;
+        final double x0 = cx + Math.sin(ssa) * ipr;
+        final double y0 = cy + Math.cos(ssa) * ipr;
+        final double x1 = cx + Math.sin(ssa) * opr;
+        final double y1 = cy + Math.cos(ssa) * opr;
+        final double x2 = cx + Math.sin(esa) * opr;
+        final double y2 = cy + Math.cos(esa) * opr;
+        final double x3 = cx + Math.sin(esa) * ipr;
+        final double y3 = cy + Math.cos(esa) * ipr;
 
-        final double[][] vertices = new double[][] {
-                {x0, y0},
-                {x1, y1},
-                {x2, y2},
-                {x3, y3}
-        };
-
+        double[][] vertices = new double[][]{{x0, y0}, {x1, y1}, {x2, y2}, {x3, y3}};
         RenderHelper.drawPoly(vertices, this.hovered ? this.hoverColor : this.color);
 
         // Uncomment to draw points in red
@@ -84,18 +72,23 @@ public class GuiRadialButton extends GuiButton {
 
     @Override
     public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
-        return this.enabled && this.visible && this.isMouseOver(mouseX, mouseY);
+        return this.enabled && this.visible && this.isMouseOver(mc, mouseX, mouseY);
     }
 
-    private boolean isMouseOver(int mouseX, int mouseY) {
-        final double sliceAngle = Math.toRadians(360) / this.sliceCount;
-        final double beginAngle = sliceAngle * this.sliceNum - sliceAngle / 2;
-        final double endAngle = sliceAngle * this.sliceNum + sliceAngle / 2;
+    private boolean isMouseOver(Minecraft mc, int mouseX, int mouseY) {
+        final ScaledResolution scaledRes = new ScaledResolution(mc);
 
-        final double polarRadius = Math.hypot(mouseX, mouseY);
-        final double angle = Math.atan2(mouseY, mouseX);
+        final double cx = scaledRes.getScaledWidth_double() / 2;
+        final double cy = scaledRes.getScaledHeight_double() / 2;
 
-        return angle >= beginAngle && angle <= endAngle && polarRadius <= this.radius + this.thickness && polarRadius >= this.deadRadius;
+        final double sa = Math.PI * 2 / this.sliceCount; // Slice angle
+        final double ssa = (this.sliceCount - this.sliceNum) * sa - sa / 2 + Math.PI; // Start slice angle
+        final double esa = ssa + sa; // End slice angle
+
+        final double mr = Math.hypot(mouseX - cx, mouseY - cy); // Mouse radius
+        final double ma = Math.atan2(mouseX - cx, mouseY - cy); // Mouse angle
+
+        return MathHelper.angleBetween(ssa, esa, ma) && mr > this.deadRadius;
     }
 
     @Override
