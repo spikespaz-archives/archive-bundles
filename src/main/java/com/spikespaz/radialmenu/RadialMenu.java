e04e709477e1d90845f7ed2e37595b9792103eba
BuildTools <spikespaz@outlook.com>
2020-04-05 20:08:16 -0700

    Use Lumbok getter for mod proxy

--- src/main/java/com/spikespaz/radialmenu/RadialMenu.java
+++ src/main/java/com/spikespaz/radialmenu/RadialMenu.java
@@ -1,25 +1,20 @@
 package com.spikespaz.radialmenu;
 
 import com.spikespaz.radialmenu.proxy.IProxy;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import lombok.Getter;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod(
-    modid = RadialMenu.MOD_ID,
-    name = RadialMenu.MOD_NAME,
-    version = RadialMenu.VERSION
-)
+@Mod(modid = RadialMenu.MOD_ID, name = RadialMenu.MOD_NAME, version = RadialMenu.VERSION)
 public class RadialMenu {
     public static final String MOD_ID = "radialmenu";
     public static final String MOD_NAME = "Radial Menu";
     public static final String VERSION = "0.1.0";
 
+    @Getter
     @SidedProxy(clientSide = "com.spikespaz.radialmenu.proxy.ClientProxy")
     private static IProxy proxy;
 
@@ -39,9 +34,5 @@ public class RadialMenu {
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         proxy.postInit(event);
-    }
-
-    public IProxy getProxy() {
-        return proxy;
     }
 }
