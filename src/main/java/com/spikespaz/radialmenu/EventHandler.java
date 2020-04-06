40470e3274ce75aa51a824d2690cd270c09ac3d7
BuildTools <spikespaz@outlook.com>
2020-04-06 00:18:14 -0700

    Move key binding to own class

--- src/main/java/com/spikespaz/radialmenu/EventHandler.java
+++ src/main/java/com/spikespaz/radialmenu/EventHandler.java
@@ -4,6 +4,10 @@ import com.spikespaz.radialmenu.gui.GuiRadialMenu;
 import com.spikespaz.radialmenu.proxy.ClientProxy;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -14,14 +18,14 @@ public class EventHandler {
     private boolean openGuiKeyPressed = false;
 
     @SideOnly(Side.CLIENT)
-    @SubscribeEvent()
+    @SubscribeEvent
     public void onEvent(RenderGameOverlayEvent.Pre event) {
         if (mc.currentScreen instanceof GuiRadialMenu && event.getType() == RenderGameOverlayEvent.ElementType.CROSSHAIRS)
             event.setCanceled(true);
     }
 
     @SideOnly(Side.CLIENT)
-    @SubscribeEvent()
+    @SubscribeEvent
     public void onEvent(RenderGameOverlayEvent.Post event) {
         if (this.openGuiKeyPressed && event.getType() == RenderGameOverlayEvent.ElementType.ALL) {
             mc.displayGuiScreen(new GuiRadialMenu(mc));
@@ -30,8 +34,8 @@ public class EventHandler {
     }
 
     @SideOnly(Side.CLIENT)
-    @SubscribeEvent()
+    @SubscribeEvent
     public void onEvent(InputEvent.KeyInputEvent event) {
-        this.openGuiKeyPressed = ClientProxy.keyBindings.get(ClientProxy.GUI_OPEN_KEY).isPressed();
+        this.openGuiKeyPressed = KeyBindings.openMenu0.isPressed();
     }
 }
---
+++ src/main/java/com/spikespaz/radialmenu/KeyBindings.java
@@ -0,0 +1,31 @@
+package com.spikespaz.radialmenu;
+
+import lombok.SneakyThrows;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import org.lwjgl.input.Keyboard;
+
+import java.lang.reflect.Field;
+
+public class KeyBindings {
+    private static final String LANG_PREFIX = "key." + RadialMenu.MOD_ID;
+
+    private static final String CATEGORY = LANG_PREFIX + ".category";
+
+    public static KeyBinding openMenu0;
+
+    public static void initialize() {
+        openMenu0 = new KeyBinding(LANG_PREFIX + ":open_menu_0", KeyConflictContext.IN_GAME, Keyboard.KEY_X, CATEGORY);
+    }
+
+    @SneakyThrows
+    public static void register() {
+        for (Field field : KeyBindings.class.getDeclaredFields()) {
+            if (field.getType() == KeyBinding.class) {
+                field.setAccessible(true);
+                ClientRegistry.registerKeyBinding((KeyBinding) field.get(null));
+            }
+        }
+    }
+}
