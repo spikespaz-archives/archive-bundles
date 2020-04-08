a28a80ad8083bd42d039a5e0bd4a94dcf2c2b193
BuildTools <spikespaz@outlook.com>
2020-04-07 20:37:52 -0700

    Add explicit opacity options to config

--- src/main/java/com/spikespaz/radialmenu/ConfigHandler.java
+++ src/main/java/com/spikespaz/radialmenu/ConfigHandler.java
@@ -22,16 +22,26 @@ public class ConfigHandler {
     public static int deadZoneRadius = 30;
 
     @Config.LangKey(LANG_KEY_PREFIX + ".label_bg_color")
-    @Config.Comment({"Background color of the label in the center of the radial menu.", "ARGB in hexadecimal format (AARRGGBB)."})
-    public static String labelBgColor = "AA000000";
+    @Config.Comment({"Background color of the label in the center of the radial menu.", "RGB in hexadecimal format (RRGGBB)."})
+    public static String labelBgColor = "000000";
+
+    @Config.LangKey(LANG_KEY_PREFIX + ".label_bg_opacity")
+    @Config.Comment("Background opacity of the label in the center of the radial menu.")
+    @Config.RangeInt(min = 0, max = 1)
+    public static double labelBgOpacity = 0.75;
 
     @Config.LangKey(LANG_KEY_PREFIX + ".label_text_color")
-    @Config.Comment({"Text color of the label in the center of the radial menu.", "ARGB in hexadecimal format (AARRGGBB)."})
-    public static String labelTextColor = "FFFFFFFF";
+    @Config.Comment({"Text color of the label in the center of the radial menu.", "RGB in hexadecimal format (RRGGBB)."})
+    public static String labelTextColor = "FFFFFF";
+
+    @Config.LangKey(LANG_KEY_PREFIX + ".label_text_opacity")
+    @Config.Comment("Text opacity of the label in the center of the radial menu.")
+    @Config.RangeInt(min = 0, max = 1)
+    public static double labelTextOpacity = 1.0;
 
     @Config.LangKey(LANG_KEY_PREFIX + ".label_text_empty_color")
-    @Config.Comment({"Text color of the label in the center of the radial menu when it is empty (button unassigned).", "ARGB in hexadecimal format (AARRGGBB)."})
-    public static String labelTextEmptyColor = "FFFE3F3F";
+    @Config.Comment({"Text color of the label in the center of the radial menu when it is empty (button unassigned).", "RGB in hexadecimal format (RRGGBB)."})
+    public static String labelTextEmptyColor = "FE3F3F";
 
     @Getter
     @Config.LangKey(LANG_KEY_PREFIX + ".label_padding_x")
@@ -46,12 +56,22 @@ public class ConfigHandler {
     public static int labelPaddingY = 4;
 
     @Config.LangKey(LANG_KEY_PREFIX + ".button_bg_color")
-    @Config.Comment({"Background color of each radial button.", "ARGB in hexadecimal format (AARRGGBB)."})
-    public static String buttonBgColor = "AA000000";
+    @Config.Comment({"Background color of each radial button.", "RGB in hexadecimal format (RRGGBB)."})
+    public static String buttonBgColor = "000000";
+
+    @Config.LangKey(LANG_KEY_PREFIX + ".button_bg_opacity")
+    @Config.Comment("Background opacity of each radial button.")
+    @Config.RangeInt(min = 0, max = 1)
+    public static double buttonBgOpacity = 0.75;
 
     @Config.LangKey(LANG_KEY_PREFIX + ".button_bg_hover_color")
-    @Config.Comment({"Background color of each radial button when it is hovered or highlighted.", "ARGB in hexadecimal format (AARRGGBB)."})
-    public static String buttonBgHoverColor = "CCCC0000";
+    @Config.Comment({"Background color of each radial button when it is hovered or highlighted.", "RGB in hexadecimal format (RRGGBB)."})
+    public static String buttonBgHoverColor = "CC0000";
+
+    @Config.LangKey(LANG_KEY_PREFIX + ".button_bg_hover_opacity")
+    @Config.Comment("Background opacity of each radial button when it is hovered or highlighted.")
+    @Config.RangeInt(min = 0, max = 1)
+    public static double buttonBgHoverOpacity = 1.0;
 
     @Getter
     @Config.LangKey(LANG_KEY_PREFIX + ".button_thickness")
@@ -60,22 +80,22 @@ public class ConfigHandler {
     public static int buttonThickness = 30;
 
     public static int getLabelBgColor() {
-        return (int) Long.parseLong(labelBgColor, 16);
+        return (int) (labelBgOpacity * 255) << 24 | (int) Long.parseLong(labelBgColor, 16);
     }
 
     public static int getLabelTextColor() {
-        return (int) Long.parseLong(labelTextColor, 16);
+        return (int) (labelTextOpacity * 255) << 24 | (int) Long.parseLong(labelTextColor, 16);
     }
 
     public static int getLabelTextEmptyColor() {
-        return (int) Long.parseLong(labelTextEmptyColor, 16);
+        return (int) (labelTextOpacity * 255) << 24 | (int) Long.parseLong(labelTextEmptyColor, 16);
     }
 
     public static int getButtonBgColor() {
-        return (int) Long.parseLong(buttonBgColor, 16);
+        return (int) (buttonBgOpacity * 255) << 24 | (int) Long.parseLong(buttonBgColor, 16);
     }
 
     public static int getButtonBgHoverColor() {
-        return (int) Long.parseLong(buttonBgHoverColor, 16);
+        return (int) (buttonBgHoverOpacity * 255) << 24 | (int) Long.parseLong(buttonBgHoverColor, 16);
     }
 }
