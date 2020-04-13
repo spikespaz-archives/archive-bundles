a19051d385cbfa97be3dfd106092a59f827122c2
BuildTools <spikespaz@outlook.com>
2020-04-13 16:52:01 -0700

    Add addition and removal of radial buttons in GuiEditRadialMenu

--- src/main/java/com/spikespaz/radialmenu/gui/GuiEditRadialMenu.java
+++ src/main/java/com/spikespaz/radialmenu/gui/GuiEditRadialMenu.java
@@ -8,7 +8,13 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 public class GuiEditRadialMenu extends GuiRadialMenu {
+    private static final int ADD_BUTTON = 101;
+    private static final int DELETE_BUTTON = 102;
+    private static final int btnW = 150;
+    private static final int btnH = 20;
     protected int editsX;
+    private boolean reInitGui;
+    private int lastSelectedButtonId;
 
     public GuiEditRadialMenu(Minecraft mc) {
         super(mc);
@@ -25,7 +31,36 @@ public class GuiEditRadialMenu extends GuiRadialMenu {
     }
 
     @Override
+    public void initGui() {
+        super.initGui();
+
+//        this.addButton(new GuiButton(100, this.editsX - btnW / +2, this.height / 2 - btnH / 2, btnW, btnH, "Add Button"));
+        this.addButton(new GuiButton(ADD_BUTTON, this.width / 2 + 4, this.height - btnH - 4, btnH, btnH, "+"));
+        this.addButton(new GuiButton(DELETE_BUTTON, this.width / 2 + 8 + btnH, this.height - btnH - 4, btnH, btnH, "-"));
+
+        this.menuX = this.width / 4;
+        this.menuY = this.height / 2;
+        this.editsX = (this.width / 4) * 3;
+
+        for (GuiButton button : this.buttonList)
+            if (button instanceof GuiRadialButton) {
+                if (button.id == this.lastSelectedButtonId)
+                    ((GuiRadialButton) button).setSelected(true);
+
+                ((GuiRadialButton) button).cx = this.menuX;
+                ((GuiRadialButton) button).cy = this.menuY;
+            } else if (button != null) {
+//                button.x = this.editsX - button.width / 2;
+            }
+    }
+
+    @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        if (this.reInitGui) {
+            this.initGui();
+            this.reInitGui = false;
+        }
+
         this.drawDefaultBackground();
 
         super.drawScreen(mouseX, mouseY, partialTicks);
@@ -38,16 +73,35 @@ public class GuiEditRadialMenu extends GuiRadialMenu {
 
     @Override
     protected void actionPerformed(GuiButton guiButton) {
-        GuiRadialButton button = (GuiRadialButton) guiButton;
+        if (guiButton instanceof GuiRadialButton) {
+            GuiRadialButton button = (GuiRadialButton) guiButton;
 
 //        if (ConfigHandler.SOUND.isButtonSoundEnabled())
 //            button.playPressSound(this.mc.getSoundHandler());
 
-        button.setSelected(true);
+            button.setSelected(true);
+            this.lastSelectedButtonId = button.id;
 
-        for (GuiButton guiButton1 : this.buttonList)
-            if (!guiButton.equals(guiButton1))
-                ((GuiRadialButton) guiButton1).setSelected(false);
+            for (GuiButton guiButton1 : this.buttonList)
+                if ((guiButton1 instanceof GuiRadialButton) && !guiButton.equals(guiButton1))
+                    ((GuiRadialButton) guiButton1).setSelected(false);
+        } else {
+            GuiRadialButton radialBtn = this.getSelectedButton();
+            this.lastSelectedButtonId = radialBtn.id;
+
+            switch (guiButton.id) {
+                case ADD_BUTTON:
+                    keyBindings.add(radialBtn.id, null);
+                    buttonIcons.add(radialBtn.id, null);
+                    this.reInitGui = true;
+                    break;
+                case DELETE_BUTTON:
+                    keyBindings.remove(radialBtn.id);
+                    buttonIcons.remove(radialBtn.id);
+                    this.reInitGui = true;
+                    break;
+            }
+        }
     }
 
     @Override
@@ -75,19 +129,5 @@ public class GuiEditRadialMenu extends GuiRadialMenu {
         bufferbuilder.pos(x1, y0, 0.0D).tex(width / 32f, 0).color(64, 64, 64, 255).endVertex();
         bufferbuilder.pos(x0, y0, 0.0D).tex(0, 0).color(64, 64, 64, 255).endVertex();
         tessellator.draw();
-    }
-
-    @Override
-    public void setWorldAndResolution(Minecraft mc, int width, int height) {
-        super.setWorldAndResolution(mc, width, height);
-
-        this.menuX = this.width / 4;
-        this.menuY = this.height / 2;
-        this.editsX = Math.min(this.width - 210, this.width / 4);
-
-        for (GuiButton button : this.buttonList) {
-            ((GuiRadialButton) button).cx = this.menuX;
-            ((GuiRadialButton) button).cy = this.menuY;
-        }
     }
 }
