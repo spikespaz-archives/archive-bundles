5e4d9d177054255fc143191bae8bf631f642c4c6
BuildTools <spikespaz@outlook.com>
2020-04-29 23:50:26 -0700

    Add implementation of button as label (WIP)

--- src/main/java/com/spikespaz/radialmenu/gui/widgets/ButtonWidget.java
+++ src/main/java/com/spikespaz/radialmenu/gui/widgets/ButtonWidget.java
@@ -1,11 +1,35 @@
 package com.spikespaz.radialmenu.gui.widgets;
 
+import lombok.Getter;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 
-public class ButtonWidget extends LabelWidget {
-    public ButtonWidget(FontRenderer fontRenderer) {
-        super(fontRenderer);
+public class ButtonWidget extends Widget {
+    protected final Minecraft mc;
+    protected final FontRenderer fontRenderer;
+    @Getter
+    protected LabelWidget labelWidget;
+
+    public ButtonWidget(Minecraft mc, FontRenderer fontRenderer) {
+        super();
+        this.mc = mc;
+        this.fontRenderer = fontRenderer;
+        this.labelWidget = LabelWidget.build(this.fontRenderer).box(this.width, this.height, this.x, this.y).done();
+        this.debugColor = 0x440066FF;
     }
 
+    @Override
+    public void draw(double mouseX, double mouseY, float partialTicks) {
+        if (!this.visible)
+            return;
 
+        super.draw(mouseX, mouseY, partialTicks);
+
+        this.labelWidget.draw(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    public void update() {
+        this.labelWidget.setBox(this.width, this.height, this.x, y);
+    }
 }
